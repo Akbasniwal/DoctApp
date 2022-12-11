5d89bf50b4c7da48aa1cbd6c31c7f9309c9c59a3
Akbasniwal <akbasniwal2907@gmail.com>
2022-12-11 22:12:53 +0530

    model test

--- tests/test_urls.py
+++ tests/test_urls.py
@@ -1,9 +1,20 @@
 from django.test import SimpleTestCase
 from django.urls import reverse, resolve
-from home.views import home, search, login
+from home.views import home, search, plogin, dlogin
 
 
 class TestUrls(SimpleTestCase):
-    def test_home_url_is_resolved(self):
-        url = reverse('list')
+    def test_home_url_resolved(self):
+        url = reverse('home')
         print(resolve(url))
+        self.assertEquals(resolve(url).func, home)
+
+    def test_search_url_resolved(self):
+        url = reverse('search')
+        print(resolve(url))
+        self.assertEquals(resolve(url).func, search)
+
+    def test_login_url_resolved(self):
+        url = reverse('plogin')
+        print(resolve(url))
+        self.assertEquals(resolve(url).func, plogin)
